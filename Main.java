c50036d3733ffa0b9d42535beba38b3589e2083b
Davi Pereira <davirpereira2@gmail.com>
2026-07-14 22:05:15 +0000

    Acrescentei as funções corretas aos espaços .

--- Main.java
+++ Main.java
@@ -1,10 +1,7 @@
+
+
 public class Main {
-    public static void main(String[] args) {
-        Personagem heroi1 = new Personagem("Arthus", "Guerreiro", 1, 100, 10.5);
+ public static void main(String[] args) {
+ Personagem heroi1 = new Personagem("Arthus", "Guerreiro", 1, 100, 10.5);
 
-        .
-        .
-        .
-
-    }
-}
+ }
--- Personagem.java
+++ Personagem.java
@@ -7,17 +7,22 @@ public class Personagem {
 
     public Personagem(String nome, String classe, int nivel, int pontosDeVida, double poderBase) {
         this.nome = nome;
-        .
-        .
-        .
+        this.classe = classe;
+        this.nivel = nivel ;
+        this.pontosDeVida = pontosDeVida ;
+        this.poderBase = poderBase ;
 
     }
 
     public void exibirStatus() {
         System.out.println("--- Status do Personagem ---");
-        .
-        .
-        .
+        System.out.println( "Nome:" + nome);
+        System.out.println("Classe:" + classe);
+        System.out.println("Nível:" + nivel);
+        System.out.println("Pontos de vida:" + pontosDeVida);
+        System.out.println("Poder base:" + poderBase);
+
+        
 
     }
 }
